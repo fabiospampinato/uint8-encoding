75e61f15b8c13c53da477814f4b1b7a98e9a9605
Fabio Spampinato <spampinabio@gmail.com>
2021-08-14 18:39:40 +0200

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,30 @@
+
+/* IMPORT */
+
+const {describe} = require ( 'fava' );
+const fc = require ( 'fast-check' );
+const {default: U8} = require ( '../dist' );
+
+/* MAIN */
+
+describe ( 'Uint8 Encoding', it => {
+
+  it ( 'works with fc-generated codepoints', t => {
+
+    const assert = str => t.is ( U8.decode ( U8.encode ( str ) ), str );
+    const property = fc.property ( fc.fullUnicode (), assert );
+
+    fc.assert ( property, { numRuns: 1000000 } );
+
+  });
+
+  it ( 'works with fc-generated strings', t => {
+
+    const assert = str => t.is ( U8.decode ( U8.encode ( str ) ), str );
+    const property = fc.property ( fc.fullUnicodeString (), assert );
+
+    fc.assert ( property, { numRuns: 1000000 } );
+
+  });
+
+});
